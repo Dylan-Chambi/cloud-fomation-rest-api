704fffc9faf3aaa73c524a2d2a4cd530acaa8e72
Dylan-Chambi <dylanchambifi@gmail.com>
2022-06-22 02:53:37 -0400

    Change response from lambda

--- CloudFormation/src/app.py
+++ CloudFormation/src/app.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print(body)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('This is a response from the Lambda function.')
     }
